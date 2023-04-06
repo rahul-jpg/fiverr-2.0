47c79ec605e675aa70215e74afbbe32fac822f44
Rahul Sarkar <8199829rs@gmail.com>
2023-04-06 11:40:29 +0530

    first commit

---
+++ src/components/Utils/Pagination.js
@@ -0,0 +1,23 @@
+import ChevronLeftIcon from "@heroicons/react/24/outline/ChevronLeftIcon"
+import ChevronRightIcon from "@heroicons/react/24/outline/ChevronRightIcon"
+
+const Pagination = ({ cardsPerPage, totalCards, paginate, currentPage, rightBtn, leftBtn }) => {
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(totalCards / cardsPerPage); i++) {
+        pageNumbers.push(i)
+    }
+    return (
+        <div className="my-9 flex justify-center">
+            <ul className="flex items-center space-x-3">
+                <li onClick={leftBtn} className="px-3 py-2 border text-lg cursor-pointer"><ChevronLeftIcon className="w-5 h-5" /></li>
+                {
+                    pageNumbers.map(num => <li onClick={() => paginate(num)} className={`transition-all ease-in-out px-4 py-1 border text-lg cursor-pointer ${currentPage === num && "font-bold"}`}>
+                        {num}
+                    </li>)
+                }
+                <li onClick={rightBtn} className="px-3 py-2 border text-lg cursor-pointer"><ChevronRightIcon className="w-5 h-5" /></li>
+            </ul>
+        </div>
+    )
+}
+export default Pagination
